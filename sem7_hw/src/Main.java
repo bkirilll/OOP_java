17b7fff8883d3bb2aea5b8598d5b3bc6ed95d1e5
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-06-30 14:25:36 +0300

    Complex calc added

---
+++ sem7_hw/src/Main.java
@@ -0,0 +1,9 @@
+import controller.Controller;
+import view.View;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller(new View());
+        controller.buttonClick();
+    }
+}
